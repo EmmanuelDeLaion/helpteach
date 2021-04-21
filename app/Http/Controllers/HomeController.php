105985892278ab7f1768666b9517c43fe4c49853
Mono <Mono@DESKTOP-BR5IUL7>
2021-04-20 22:43:17 -0500

    correccion en home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,16 +6,14 @@ use Illuminate\Http\Request;
 
 use App\Models\Course;
 
-
 class HomeController extends Controller
 {
 
     public function __invoke()
     {
-        // recuperar el registro de los cursos donde el status sea 3 o sea publicados
-        $courses = Course::where('status', '3')->get();
-
-        return $courses;
-        return view('welcome');
+        // recuperar el registro de los cursos 
+        $courses = Course::where('status','3')->latest('id')->get();
+        // return Course::find(2)->rating;
+        return view('welcome', compact('courses'));
     }
 }
