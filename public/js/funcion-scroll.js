79d22f79f74cecbbee332cb3d3cf23e46939cdf7
Mono <Mono@DESKTOP-BR5IUL7>
2021-04-21 23:12:14 -0500

    seccion cursos y modificaciones en funciones js

---
+++ public/js/funcion-scroll.js
@@ -0,0 +1,11 @@
+ 
+  
+  $(window).on('scroll', function() {
+    var scrolled = $(window).scrollTop();
+    var height = $(window).height();
+    if (scrolled < height) {
+      $('.full-image').css({
+        'top': -scrolled * .2
+      });
+    }
+  });
