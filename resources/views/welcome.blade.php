70fa361317de7e821eb4acc888c0ecd600680095
Mono <Mono@DESKTOP-BR5IUL7>
2021-04-26 18:53:22 -0500

    cards estilos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -120,15 +120,12 @@
                 </div>
             </div>
 
-            <div class=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid-cols-4 gap-x-6 gap-y-8">
-
+            <div class="grid gap-4 grid-cols-4 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8  gap-x-6 gap-y-8">
 
                 @foreach ($courses as $course)
 
                     {{-- {{ Storage::url($course->image->url) }} --}}
-
-
-                    <article class="card-cursos card-1">
+                    <article class="card-cursos mb-12">
                         <div class="card-info-hover">
                             <svg class="card-like" viewBox="0 0 24 24">
                                 <path fill="#000000"
@@ -149,16 +146,48 @@
                             <h3 class="card-title"> {{ $course->title }} </h3>
                             <span class="card-by">De: <a href="#" class="card-author" title="author">
                                     {{ $course->teacher->name }} </a></span>
+
+
+                            <div class="flex justify-between">
+                                <ul class="rating flex text-sm">
+                                    <li> <i
+                                            class="fas fa-star text-{{ $course->rating >= 1 ? 'yellow' : 'gray' }}-400 "></i>
+                                    </li>
+                                    <li> <i
+                                            class="fas fa-star text-{{ $course->rating >= 2 ? 'yellow' : 'gray' }}-400 "></i>
+                                    </li>
+                                    <li> <i
+                                            class="fas fa-star text-{{ $course->rating >= 3 ? 'yellow' : 'gray' }}-400 "></i>
+                                    </li>
+                                    <li> <i
+                                            class="fas fa-star text-{{ $course->rating >= 4 ? 'yellow' : 'gray' }}-400 "></i>
+                                    </li>
+                                    <li> <i
+                                            class="fas fa-star text-{{ $course->rating >= 5 ? 'yellow' : 'gray' }}-400 "></i>
+                                    </li>
+                                </ul>
+
+                                <p class="text-sm text-gray">
+                                    <i class=" icon-students fas fa-chalkboard-teacher"></i>
+                                    ({{ $course->students_count }})
+                                </p>
+
+                            </div>
+
+
                         </div>
 
+
+
+
                         <div class="flex justify-center">
+
                             <button onclick="location.href='{{ route('about.index') }}'" type="submit"
-                                class="w-11/12	 btn-ver-curso mb-12 text-center">
+                                class="w-11/12 btn-ver-curso mb-12 text-center">
                                 Más información
                             </button>
                         </div>
                     </article>
-
 
                 @endforeach
 
