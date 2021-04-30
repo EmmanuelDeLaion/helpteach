bed4a3312716a81a05fac5da0ea08c69f9b66cc9
Mono <Mono@DESKTOP-BR5IUL7>
2021-04-30 13:50:43 -0500

    correccion diseño login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -78,19 +78,7 @@
                 </div>
                 <img src="img/log.svg" class="image" alt="" />
             </div>
-            <div class="panel right-panel">
-                <div class="content">
-                    <h3>One of us ?</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-                        laboriosam ad deleniti.
-                    </p>
-                    <button class="btn transparent" id="sign-in-btn">
-                        Sign in
-                    </button>
-                </div>
-                <img src="img/register.svg" class="image" alt="" />
-            </div>
+          
         </div>
     </div>
 
