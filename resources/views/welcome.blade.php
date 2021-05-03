bc39b384f555ae6ce87cfbc9237991cb46a4d941
Mono <Mono@DESKTOP-BR5IUL7>
2021-05-03 17:16:45 -0500

    responsive y modificacion en footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,7 +66,8 @@
                 <h1 class="text-center mt-6 titulo-numeros"> Estudia y practica en <span class="span-numeros">
                         HelpTeach</span> </h1>
 
-                <div class="  justify-center mx-auto grid grid-cols-1 lg:grid-cols-4 px-4 mt-6">
+                <div
+                    class="justify-center mx-auto grid grid-cols-1 lg:grid-cols-4 xl:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 px-4 mt-2">
                     <div class="contador">
                         <h1 class="text-center titulos-numeros contador_cantidad" id="cont1" data-cantidad-total="105">
                             0</h1>
@@ -74,19 +75,19 @@
                     </div>
 
                     <div class="contador">
-                        <h1 class="text-center titulos-numeros contador_cantidad" id="cont2"
-                            data-cantidad-total="20">0</h1>
+                        <h1 class="text-center titulos-numeros contador_cantidad" id="cont2" data-cantidad-total="20">0
+                        </h1>
                         <p class="text-center descripcion-numeros">Alumnos instructores</p>
                     </div>
 
 
                     <div class="contador">
-                        <h1 class="text-center titulos-numeros contador_cantidad" id="cont3"
-                            data-cantidad-total="70">0</h1>
+                        <h1 class="text-center titulos-numeros contador_cantidad" id="cont3" data-cantidad-total="70">0
+                        </h1>
                         <p class="text-center descripcion-numeros">Cursos disponibles</p>
                     </div>
 
-                    <div>
+                    <div class="flex justify-center items-center">
                         <button class="btn-primario-outline">Ver más </button>
                     </div>
 
@@ -98,7 +99,7 @@
 
 
         <section class="seccion-conteiner">
-            <h1 class="text-center mb-12 mt-24  h1-titulo-enseñar">Contenido</h1>
+            <h1 class="text-center mb-12 mt-6  h1-titulo-enseñar">Contenido</h1>
             <div
                 class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 xl:grid-cols-3 gap-6  ">
                 <div class="card">
