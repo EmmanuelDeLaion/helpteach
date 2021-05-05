93f8912c365632b84bdfe980c8aad0efe2673083
Mono <Mono@DESKTOP-BR5IUL7>
2021-05-05 01:25:56 -0500

    componente livewire seccion ver cursos

---
+++ app/Http/Livewire/SectionSeeCourses.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SectionSeeCourses extends Component
+{
+    public function render()
+    {
+        return view('livewire.section-see-courses');
+    }
+}
