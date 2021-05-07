809454e85001260c528ef46aaf4ff18fd4a0e2b8
Mono <Mono@DESKTOP-BR5IUL7>
2021-05-07 01:01:37 -0500

    slider cursos courses

---
+++ resources/views/livewire/ad.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <section class="anuncio mt-36">
+        <div class="container-anuncio">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
+                <h1 class="titulo-anuncios">¿Dificultades al entender <br> una materia?</h1>
+                <p class="text-white">En <span>HelpTeach</span> podrás encontrar cursos sobre la materia estes
+                    cursando en tu
+                    universidad</p>
+                <button class="btn-secundario">Aprende ahora</button>
+            </div>
+
+            <img class="img-anuncio" src="{{ asset('/images/anuncios/persona2.png') }}" alt="">
+        </div>
+    </section>
+
+</div>
