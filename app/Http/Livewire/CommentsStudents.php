ce017a068ca5d1f252d7ebb8d331ca575900f1df
Mono <Mono@DESKTOP-BR5IUL7>
2021-05-06 19:45:13 -0500

    agregacion de componentes livewire en home y curso

---
+++ app/Http/Livewire/CommentsStudents.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CommentsStudents extends Component
+{
+    public function render()
+    {
+        return view('livewire.comments-students');
+    }
+}
---
+++ app/Http/Livewire/SectionStartNow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SectionStartNow extends Component
+{
+    public function render()
+    {
+        return view('livewire.section-start-now');
+    }
+}
