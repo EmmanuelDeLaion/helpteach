9feb970fcb8c8ae428c46f2b05f165da10c929c7
Mono <Mono@DESKTOP-BR5IUL7>
2021-05-08 00:41:21 -0500

    portada show course

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Course;
+
 class CourseController extends Controller
 {
     public function index(){
         return view('courses.index');
+    }
+
+    public function show(Course $course){
+        return view('courses.show', compact('course'));
     }
 }
--- app/Models/Course.php
+++ app/Models/Course.php
@@ -105,7 +105,7 @@ class Course extends Model
 
     public function category()
     {
-        return $this->belongsTo('App\Models\Level');
+        return $this->belongsTo('App\Models\Category');
     }
 
     public function price()
