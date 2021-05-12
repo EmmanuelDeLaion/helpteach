697a9aec4ce6c73afaa42f3be973c2010163e98d
Mono <Mono@DESKTOP-BR5IUL7>
2021-05-12 18:50:18 -0500

    seccion dudas

---
+++ resources/views/livewire/doubts.blade.php
@@ -0,0 +1,23 @@
+<div>
+
+    <div class="content-doubts">
+        <h1 class="titulo-secciones text-center">¿Tienes dudas?</h1>
+        <p class="subtitle-doubts text-center">Contacta a los instructores</p>
+        <div class="flex justify-center">
+            <img class="avatars-doubts" src="{{ asset('/images/cursos/avatars-instructores.png') }}" alt="">
+
+
+        </div>
+        <div class="mx-auto px-4 sm:px-6 lg:px-8 ">
+            <p class="textos text-center">Es importante para la comunidad de Alumnos que se sientan seguros
+                de la enseñanza <br> que están adquiriendo para ello siempre habrá una comunicación
+                con los instructores alumnos</p>
+        </div>
+
+
+        <div class="flex justify-center mt-6">
+            <button class="btn-primario">Contacta</button>
+        </div>
+    </div>
+
+</div>
