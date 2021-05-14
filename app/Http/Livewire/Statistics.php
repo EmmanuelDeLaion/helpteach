c8cce2740859c3bd489326e8b868878c4e346420
Mono <Mono@DESKTOP-BR5IUL7>
2021-05-14 00:02:23 -0500

    cuerpo quienes somos

---
+++ app/Http/Livewire/Statistics.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Statistics extends Component
+{
+    public function render()
+    {
+        return view('livewire.statistics');
+    }
+}
--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -41,9 +41,58 @@
             </div>
         </section>
 
+        <section class="container mt-6">
+
+            <div class="flex justify-center">
+                <img width="200" src="{{ asset('/images/logo.png') }}" alt="">
+            </div>
 
 
-        @livewire('section-see-courses')
+            <div class="flex justify-center mt-6">
+                <h1 class="w-full md:w-3/4 lg:w-3/4 text-center titulo-cursos-home">Plataforma de cursos en linea hechos
+                    por <span class="span-primario">alumnos</span>  para
+                    <span class="span-primario"> alumnos </span> </h1>
+            </div>
+
+            <div class="flex justify-center">
+                <p class="w-full md:w-1/2 lg:w-3/4 text-sm text-gray-500 mt-12">La idea principal de este proyecto es
+                    motivar a los alumnos de la
+                    carrera
+                    de ingeniería en sistemas
+                    computacionales (actualmente) que sigan el camino de la enseñanza sin que existan decaídas
+                    principalmente a la hora de entender los temas y no hacer complicado el buscar información en sitios
+                    donde cobran una cantidad excesiva. Para ello se pensó en hacer una plataforma totalmente gratuita
+                    (Puede haber donaciones) para los alumnos que estén inscritos en el Tec Mante donde el contenido
+                    principal de la plataforma son los videos cursos sobre los diferentes temas de materias de la
+                    carrera,
+                    así como publicaciones de dudas que existan (fotos). <br>
+                    Para ello la idea es que los alumnos a partir del
+                    primer semestre, realicen videos sobre temas en especifico de las materias que estén cursando
+                    actualmente o de las que ya cursaron anteriormente (No obligatorio).
+                    <br>
+                    <br>
+                    Todo esto con el fin de armar una comunidad donde haya comunicación y sobresalga el aprendizaje
+                    individual de los alumnos de semestres superiores ayudando así a las nuevas generaciones.
+                </p>
+            </div>
+        </section>
+
+        @livewire('statistics')
+
+        @livewire('doubts')
+
+        <section class="container mt-12  ">
+            <h1 class="titulo-secciones text-center">Te damos la bienvenida</h1>
+
+            <div class="flex justify-center mt-6 mb-6">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/t1r79hMbvlk"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+            </div>
+
+        </section>
+
         @livewire('footer')
 
 
