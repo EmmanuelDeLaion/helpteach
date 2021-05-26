1dcb20fa7a5d1c9eb69cbe74c304607930009642
Mono <Mono@DESKTOP-BR5IUL7>
2021-05-26 05:13:49 -0500

    indices anterior y next en control del curso

--- resources/views/livewire/course-status.blade.php
+++ resources/views/livewire/course-status.blade.php
@@ -13,21 +13,49 @@
 
     <div class="container grid grid-cols-3 gap-8  	">
         <div class="col-span-2 mt-6">
-            <h1>hola</h1>
+            {!! $current->frame !!}
+            {{ $current->name }}
+            <p>inidice: {{ $index }} </p>
+
+            <p>previous: {{ $previous->id }}</p>
+
+            <p>next: {{ $next->id }}</p>
         </div>
         <div class="col-span-1 mt-6">
-            <div class="contenido-curso">
-                <div
-                    class="border-l-4 border-blue-400 flex flex-col relative items-center w-full rounded-lg bg-white shadow-lg pb-5 text-gray-800 mb-10">
-                    <img class="rounded-full mt-6 w-20" src="{{ $course->teacher->profile_photo_url }}" alt="">
-                    <p class="span-primario text-sm font-bold"> <i class="fas fa-chalkboard-teacher"></i> Instructor
-                        del curso</p>
-                    <h4 class="text-gray-700 text-sm"> {{ $course->teacher->name }} </h4>
-                    <a href="" class="text-sm span-primario"> {{ '@' . Str::slug($course->teacher->name, '') }}
-                    </a>
-                    
-                </div>
+            <div
+                class="container flex flex-col relative items-center w-full rounded-lg bg-white shadow-lg pb-5 text-gray-800 mb-10">
+                <img class="rounded-full mt-6 w-20" src="{{ $course->teacher->profile_photo_url }}" alt="">
+                <p class="span-primario text-sm font-bold"> <i class="fas fa-chalkboard-teacher"></i> Instructor
+                    del curso</p>
+                <h4 class="text-gray-700 text-sm"> {{ $course->teacher->name }} </h4>
+                <a href="" class="text-sm span-primario"> {{ '@' . Str::slug($course->teacher->name, '') }}
+                </a>
+
+                <br>
+
+                <ul>
+                    @foreach ($course->sections as $section)
+                        <li>
+                            <a class="font-bold">{{ $section->name }}</a>
+                            <ul>
+                                @foreach ($section->lessons as $lesson)
+                                    <li>
+                                        <a class="cursor-pointer"
+                                            wire:click="changeLesson({{ $lesson }})">{{ $lesson->id }}
+                                            {{-- si el valor booleano es true imprime como completado si es falso no se imprime nada --}}
+                                            @if ($lesson->completed)
+                                                (Completado)
+                                            @endif
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endforeach
+                </ul>
+
             </div>
+
         </div>
     </div>
 
