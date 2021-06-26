bffc22a148da73631ded4362cb88384dad626610
Mono <Mono@DESKTOP-BR5IUL7>
2021-06-26 00:36:26 -0500

    crud completo de lecciones

--- resources/views/livewire/instructor/courses-lesson.blade.php
+++ resources/views/livewire/instructor/courses-lesson.blade.php
@@ -44,7 +44,7 @@
 
                     <div class="mt-4 flex justify-end">
                         <button wire:click="update" class="btn-primario ">Actualizar</button>
-                        <button class="btn-primario-outline-red ml-1">Cancelar</button>
+                        <button wire:click="cancel" class="btn-primario-outline-red ml-1">Cancelar</button>
                     </div>
 
                 </div>
@@ -58,7 +58,7 @@
                             {{ $item->url }}</span> </a>
                     <div class="flex">
                         <button wire:click="edit({{ $item }})" class="btn-primario ">Editar</button>
-                        <button class="btn-primario-outline-red ml-1">Eliminar</button>
+                        <button wire:click="destroy({{ $item }})" class="btn-primario-outline-red ml-1">Eliminar</button>
                     </div>
                 </div>
 
@@ -67,4 +67,68 @@
         </div>
 
     @endforeach
+
+    <div x-data="{open:false}">
+        <h1 x-show="!open" x-on:click="open = true"
+            class="text-md font-bold text-gray-800 mt-5 p-2 my-2 cursor-pointer hover:text-purple-500"><i
+                class="fas fa-plus-circle"></i> Agregar nueva lección</h1>
+
+        <div class="shadow  rounded-lg border border-purple-400 mt-5 my-2" x-show="open">
+
+            <div class="container items-center mt-2">
+                <div class="bg-purple-500 rounded-lg lg:w-2/12 p-3 block mt-2  md:ml-2 h-full shadow-inner">
+                    <div class="text-center">
+                        <div class="text-white font-bold text-sm">Nueva lección</div>
+                    </div>
+                </div>
+                <div class="flex mt-1 items-center">
+                    <label class="w-48" for="">Nombre de la lección: </label>
+                    <input wire:model="name"
+                        class="w-full bg-white focus:outline-none text-sm text-gray-800 font-bold border rounded-sm"
+                        placeholder="Ingrese el nombre de la lección" type="text">
+                </div>
+
+                @error('name')
+                    <span class="text-xs text-red-500">Favor de ingresar el titulo de la lección</span>
+                @enderror
+
+                <div class="flex items-center mt-1">
+                    <label class="w-48">Plataforma:</label>
+                    <div class="w-full  ">
+                        <select wire:model="platform_id"
+                            class="block w-full bg-grey-lighter text-sm text-gray-800 font-bold border border-grey-lighter rounded-sm   md:w-full ">
+                            @foreach ($platforms as $platform)
+                                <option class="text-sm font-bold" value="{{ $platform->id }}">
+                                    {{ $platform->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+
+                <div class="flex mt-1 items-center">
+                    <label class="w-48">URL: </label>
+                    <input wire:model="url"
+                        class="w-full bg-white focus:outline-none text-sm text-blue-400 font-bold border rounded-sm"
+                        placeholder="Ingrese la URL del video" type="text">
+                </div>
+
+                @error('url')
+                    <span class="text-xs text-red-500">{{ $message }}</span>
+                @enderror
+            </div>
+
+
+            <div class="flex justify-end mx-2 my-2">
+                <button wire:click="store" class="  btn-primario">
+                    Agregar
+                </button>
+                <button x-on:click="open = false" class="ml-1 btn-primario-outline-red">
+                    Cancelar
+                </button>
+            </div>
+        </div>
+
+    </div>
+
 </div>
