e2c0db291c5c1ed07edeb5603b080a45bf1f1638
Mono <Mono@DESKTOP-BR5IUL7>
2021-06-29 18:02:43 -0500

    subir recursos a una leccion

---
+++ app/Http/Livewire/instructor/LessonResources.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Livewire\Instructor;
+
+use App\Models\Lesson;
+use App\Models\Resource;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Storage;
+
+class LessonResources extends Component
+{
+    use WithFileUploads;
+
+    public $lesson;
+    public $file;
+
+    public function mount(Lesson $lesson){
+        $this->lesson = $lesson;
+    }
+
+    public function render()
+    {
+        return view('livewire.instructor.lesson-resources');
+    }
+
+    public function save(){
+        $this->validate([
+            'file' => 'required'
+        ]);
+
+        $url = $this->file->store('resources');
+ 
+        $this->lesson->resource()->create([
+            'url' => $url
+        ]);
+
+        $this->lesson = Lesson::find($this->lesson->id);
+    }
+
+
+
+    public function downloadResource(){
+        return response()->download( storage_path('app/public/' . $this->lesson->resource->url) );
+    }
+
+    
+
+    public function destroy(){
+        Storage::delete($this->lesson->resource->url);
+
+        $this->lesson->resource->delete();
+
+        $this->lesson = Lesson::find($this->lesson->id);
+
+    }
+
+}
