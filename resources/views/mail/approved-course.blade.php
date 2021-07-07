dbcb98a675eea13be5b743a815e0f89240d08476
Mono <Mono@DESKTOP-BR5IUL7>
2021-07-07 11:52:43 -0500

    enviar correos de aprobacion de cursos

---
+++ resources/views/mail/approved-course.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ 
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Este es un correo de prueba</h1>
+    <p>El curso es <span> {{ $course->title }} </span> </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil deserunt adipisci praesentium voluptatem. Natus
+        molestiae soluta magni animi enim laboriosam!</p>
+</body>
+
+</html>
